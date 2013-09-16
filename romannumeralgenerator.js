fedabbc6b0e54808e718ebdbe044242d0f3eeb98
Jon Dunleavy <joeyjojosmith@gmail.com>
2013-09-16 22:31:46 +0100

    fleshed out functionality of function

--- romannumeralgenerator.js
+++ romannumeralgenerator.js
@@ -1,3 +1,52 @@
+UNIT_MAP = {
+	1: 'I',
+	2: 'II',
+	3: 'III',
+	4: 'IV',
+	5: 'V',
+	6: 'VI',
+	7: 'VII',
+	8: 'VIII',
+	9: 'IX'
+};
+
+TEN_MAP = {
+	1: 'X',
+	2: 'XX',
+	3: 'XXX',
+	4: 'XL',
+	5: 'L',
+	6: 'LX',
+	7: 'LXX',
+	8: 'LXXX',
+	9: 'XC'
+};
+
+HUNDREDS_MAP = {
+	1: 'C',
+	2: 'CC',
+	3: 'CCC',
+	4: 'CD',
+	5: 'D',
+	6: 'DC',
+	7: 'DCC',
+	8: 'DCCC',
+	9: 'CM'
+};
+
+THOUSANDS_MAP = {
+	1: 'M',
+	2: 'MM',
+	3: 'MMM'
+};
+
+INDEX_MAP = {
+	0: UNIT_MAP,
+	1: TEN_MAP,
+	2: HUNDREDS_MAP,
+	3: THOUSANDS_MAP,
+}
+
 exports.generate = function(n){
 	/*
 	* Returns a roman number based on the number, n
@@ -6,13 +55,15 @@ exports.generate = function(n){
 	var retVal = [];
 	var nStr = n.toString();
 	// put them in unit/tens/hundreds order
-	var nArr = nStr.split().reverse();
+	var nArr = nStr.split('').reverse();
 	
+
 	for (var i=0;i<nArr.length;i++){
-		retVal.push('I');
+		retVal.push(INDEX_MAP[i][parseInt(nArr[i])]);
 	}	
 
-
+	retVal.reverse();
 	
 	return retVal.join('');
 };
+
