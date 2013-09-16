28ab3fd16253aa70a6156df5f4514bdb82c56f3d
Jon Dunleavy <joeyjojosmith@gmail.com>
2013-09-16 22:16:18 +0100

    Added basic function just adding I for each character spotted

--- romannumeralgenerator.js
+++ romannumeralgenerator.js
@@ -1,4 +1,18 @@
 exports.generate = function(n){
+	/*
+	* Returns a roman number based on the number, n
+	* Assumes n is a integer
+	*/
 	var retVal = [];
+	var nStr = n.toString();
+	// put them in unit/tens/hundreds order
+	var nArr = nStr.split().reverse();
+	
+	for (var i=0;i<nArr.length;i++){
+		retVal.push('I');
+	}	
+
+
+	
 	return retVal.join('');
 };
